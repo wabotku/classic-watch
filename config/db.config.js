90361e5c6cce4ab3e2893c9f8e3a3228f0223ec5
walid <walidshr8@gmail.com>
2023-07-11 10:59:52 +0700

    db config

--- config/db.config.js
+++ config/db.config.js
@@ -3,7 +3,10 @@ module.exports = {
     USER: process.env.DBUSER_USERNAME,
     PASSWORD: process.env.DBUSER_PASS,
     DB: process.env.DBUSER_DB,
+    DB_CORE: process.env.DBCORE_DB,
+    DB_MERCHANT: process.env.DBMERCHANT_DB,
     PORT: process.env.DBUSER_PORT,
+
     dialect: 'mysql',
     pool: {
         max: 5,
